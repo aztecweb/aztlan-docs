a9df6d3e5599224041e444541c17a825ce13732f
Lucas Hart <lucas.hart@aztecweb.net>
2019-09-18 11:57:41 -0300

    Added powered by on footer

--- website/core/Footer.js
+++ website/core/Footer.js
@@ -25,15 +25,20 @@ class Footer extends React.Component {
     return (
       <footer className="nav-footer" id="footer">
         <section className="sitemap">
-          <a href={this.props.config.baseUrl} className="nav-home">
-            {this.props.config.footerIcon && (
-              <img
-                src={this.props.config.baseUrl + this.props.config.footerIcon}
-                alt={this.props.config.title}
-                width="66"
-              />
-            )}
-          </a>
+          <div className="powered-by">
+			  <h5>Powered By</h5>
+			  <a href={this.props.config.poweredByUrl} className="nav-home">
+				{this.props.config.footerIcon && (
+				<img
+					src={this.props.config.baseUrl + this.props.config.footerIcon}
+					alt={this.props.config.poweredBy}
+					target="_blank"
+					width="42"
+				/>
+				)}
+				<h3>{this.props.config.poweredBy}</h3>
+			  </a>
+		  </div>
           <div>
             <h5>Docs</h5>
             <a href={this.docUrl('installation.html', this.props.language)}>
